486a8cead36c5d76000a9cab89d4da7c6e3cd835
qinglong <qinglong@@users.noreply.github.com>
2025-04-02 07:00:43 +0800

    提交练习代码 (LeetCode主题) - LeetCode主题: 搜索 - 2025-04-02 07:00:10

---
+++ generated_code/2025-04/2025-04-02.py
@@ -0,0 +1,24 @@
+def binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        
+        # Check if the target is at mid
+        if nums[mid] == target:
+            return mid
+        
+        # If the left part is sorted
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        # If the right part is sorted
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+    return -1
