dcce4199dd6c5e789c733b0917123c98d2bc6c1a
qinglong <qinglong@@users.noreply.github.com>
2025-04-02 12:27:42 +0800

    提交练习代码 (LeetCode主题) - LeetCode主题: 排序 - 2025-04-02 12:27:39

--- generated_code/2025-04/2025-04-02.py
+++ generated_code/2025-04/2025-04-02.py
@@ -1,16 +1,23 @@
-def permute(nums):
-    def backtrack(start=0):
-        if start == len(nums):
-            res.append(nums[:])
-        for i in range(start, len(nums)):
-            nums[start], nums[i] = nums[i], nums[start]  # Swap to create permutation
-            backtrack(start + 1)  # Recurse
-            nums[start], nums[i] = nums[i], nums[start]  # Backtrack (restore state)
+class Solution:
+    def merge(self, left, right):
+        result = []
+        while left and right:
+            if left[0] < right[0]:
+                result.append(left.pop(0))
+            else:
+                result.append(right.pop(0))
+        result.extend(left or right)
+        return result
+    
+    def mergeSort(self, nums):
+        if len(nums) <= 1:
+            return nums
+        mid = len(nums) // 2
+        left = self.mergeSort(nums[:mid])
+        right = self.mergeSort(nums[mid:])
+        return self.merge(left, right)
 
-    res = []
-    backtrack()
-    return res
-
-# Example usage
-nums = [1, 2, 3]
-print(permute(nums))
+# 示例使用
+nums = [38, 27, 43, 3, 9, 82, 10]
+solution = Solution()
+print(solution.mergeSort(nums))
