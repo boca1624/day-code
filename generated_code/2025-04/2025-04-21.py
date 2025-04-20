84bcb035a54598b8dbc7f2860a4655253f114b20
qinglong <qinglong@@users.noreply.github.com>
2025-04-21 07:00:08 +0800

    提交练习代码 (LeetCode主题) - LeetCode主题: 递归 - 2025-04-21 07:00:05

---
+++ generated_code/2025-04/2025-04-21.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def maxDepth(root: TreeNode) -> int:
+    if not root:
+        return 0
+    left_depth = maxDepth(root.left)
+    right_depth = maxDepth(root.right)
+    return max(left_depth, right_depth) + 1
+
+def invertTree(root: TreeNode) -> TreeNode:
+    if not root:
+        return None
+    root.left, root.right = root.right, root.left
+    invertTree(root.left)
+    invertTree(root.right)
+    return root
