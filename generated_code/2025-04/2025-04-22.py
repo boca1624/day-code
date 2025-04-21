4fd2dea88fc2422d6c6c20559d55957a001d63f0
qinglong <qinglong@@users.noreply.github.com>
2025-04-22 07:00:09 +0800

    提交练习代码 (LeetCode主题) - LeetCode主题: 分治算法 - 2025-04-22 07:00:06

---
+++ generated_code/2025-04/2025-04-22.py
@@ -0,0 +1,28 @@
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+
+    mid = len(nums) // 2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+# 示例使用
+nums = [38, 27, 43, 3, 9, 82, 10]
+sorted_nums = merge_sort(nums)
+print(sorted_nums)
