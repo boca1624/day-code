fc3f9fde7e83702daf75c86c18086c2b8dc0e915
qinglong <qinglong@@users.noreply.github.com>
2025-04-28 07:00:09 +0800

    提交练习代码 (LeetCode主题) - LeetCode主题: 搜索 - 2025-04-28 07:00:06

---
+++ generated_code/2025-04/2025-04-28.py
@@ -0,0 +1,32 @@
+# 典型的二分查找算法实现
+def binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+# 在一个旋转排序数组中进行二分查找
+def rotated_binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            return mid
+        # 判断哪个部分是有序的
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1
