f99851ecb7dc614bd64415337f9caac50ec6f11e
qinglong <qinglong@@users.noreply.github.com>
2025-05-31 07:00:11 +0800

    提交练习代码 (LeetCode主题) - LeetCode主题: 动态规划 - 2025-05-31 07:00:08

---
+++ generated_code/2025-05/2025-05-31.py
@@ -0,0 +1,15 @@
+def maxProfit(prices):
+    n = len(prices)
+    if n == 0:
+        return 0
+
+    dp = [[0] * 2 for _ in range(n)]
+    
+    dp[0][0] = 0  # No stock on day 0
+    dp[0][1] = -prices[0]  # Buy stock on day 0
+
+    for i in range(1, n):
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])  # No stock, either carry over or sell today
+        dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])  # Hold stock, either carry over or buy today
+
+    return dp[n-1][0]
