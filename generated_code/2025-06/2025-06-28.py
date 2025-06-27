3c6f4a490c78ef523e4428526c6049ca856812bd
qinglong <qinglong@@users.noreply.github.com>
2025-06-28 07:00:14 +0800

    提交练习代码 (LeetCode主题) - LeetCode主题: 设计 - 2025-06-28 07:00:11

---
+++ generated_code/2025-06/2025-06-28.py
@@ -0,0 +1,41 @@
+class LRUCache:
+    class Node:
+        def __init__(self, key, value):
+            self.key = key
+            self.value = value
+            self.prev = None
+            self.next = None
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # key -> Node
+        self.head = self.Node(0, 0)  # dummy head
+        self.tail = self.Node(0, 0)  # dummy tail
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _add_to_front(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add_to_front(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = self.Node(key, value)
+        self.cache[key] = node
+        self._add_to_front
