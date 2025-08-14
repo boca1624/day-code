75dabefe9a08b9269b7588e484e3f386697342fa
qinglong <qinglong@users.noreply.github.com>
2025-08-15 07:00:26 +0800

    自动同步代码 - 2025-08-15 07:00:03

---
+++ generated_code/2025-08/2025-08-15.py
@@ -0,0 +1,27 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def detect_cycle(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    else:
+        return None
+
+    slow = head
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+    return slow
+
+def print_list(head):
+    current = head
+    while current:
+        print(current.val, end=" ")
+        current = current.next
+    print()
