dd0bfa37a95e5291a5126214e93ff9a793a62f0c
qinglong <qinglong@users.noreply.github.com>
2025-09-03 07:00:24 +0800

    自动同步代码 - 2025-09-03 07:00:04

---
+++ generated_code/2025-09/2025-09-03.py
@@ -0,0 +1,33 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def find_middle_of_list(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
+
+def reverse_list(head):
+    prev = None
+    current = head
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    return prev
+
+def merge_two_sorted_lists(l1, l2):
+    dummy = ListNode()
+    tail = dummy
+    while l1 and l2:
+        if l1.val < l2.val:
+            tail.next, l1 = l1, l1.next
+        else:
+            tail.next, l2 = l2, l2.next
+        tail = tail.next
+    tail.next = l1 or l2
+    return dummy.next
