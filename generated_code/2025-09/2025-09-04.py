b71b386805bfeb9cc0c019a3c3651e530a9e6805
qinglong <qinglong@users.noreply.github.com>
2025-09-04 07:00:09 +0800

    提交练习代码 (LeetCode主题) - LeetCode主题: 动态规划 - 2025-09-04 07:00:08

---
+++ generated_code/2025-09/2025-09-04.py
@@ -0,0 +1,9 @@
+def maxSubArray(nums):
+    if not nums:
+        return 0
+
+    max_current = max_global = nums[0]
+    for i in range(1, len(nums)):
+        max_current = max(nums[i], max_current + nums[i])
+        max_global = max(max_global, max_current)
+    return max_global
