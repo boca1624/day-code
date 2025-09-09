f399e25398348995bc5c0dce3b33ae7d80fff142
qinglong <qinglong@users.noreply.github.com>
2025-09-10 07:00:28 +0800

    自动同步代码 - 2025-09-10 07:00:03

---
+++ generated_code/2025-09/2025-09-10.py
@@ -0,0 +1,32 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def merge_k_sorted_lists(lists):
+    def merge_two_lists(l1, l2):
+        dummy = ListNode()
+        current = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next, l1 = l1, l1.next
+            else:
+                current.next, l2 = l2, l2.next
+            current = current.next
+        current.next = l1 or l2
+        return dummy.next
+
+    if not lists:
+        return None
+    while len(lists) > 1:
+        new_lists = []
+        for i in range(0, len(lists), 2):
+            l1, l2 = lists[i], lists[i+1] if i+1 < len(lists) else None
+            if l1 and l2:
+                new_lists.append(merge_two_lists(l1, l2))
+            elif l1:
+                new_lists.append(l1)
+            elif l2:
+                new_lists.append(l2)
+        lists = new_lists
+    return lists[0]
