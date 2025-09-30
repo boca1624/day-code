343c8acce6a15c9e7cc57b74a800c7e147994994
qinglong <qinglong@users.noreply.github.com>
2025-10-01 07:00:14 +0800

    提交练习代码 (LeetCode主题) - LeetCode主题: 数学 - 2025-10-01 07:00:11

---
+++ generated_code/2025-10/2025-10-01.py
@@ -0,0 +1,22 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def sieve_of_eratosthenes(limit):
+    sieve = [True] * (limit + 1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(limit**0.5) + 1):
+        if sieve[i]:
+            for j in range(i*i, limit + 1, i):
+                sieve[j] = False
+    return [i for i, prime in enumerate(sieve) if prime]
