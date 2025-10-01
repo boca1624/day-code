9b747817afa143cdffc44c0d647df0a642249389
qinglong <qinglong@users.noreply.github.com>
2025-10-02 07:00:13 +0800

    自动同步代码 - 2025-10-02 07:00:03

---
+++ generated_code/2025-10/2025-10-02.py
@@ -0,0 +1,31 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+    
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    merged = []
+    left_index, right_index = 0, 0
+    
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+    
+    while left_index < len(left):
+        merged.append(left[left_index])
+        left_index += 1
+    
+    while right_index < len(right):
+        merged.append(right[right_index])
+        right_index += 1
+    
+    return merged
