5f01ae8467380170fde3b88d7072b6a8d11065ec
qinglong <qinglong@users.noreply.github.com>
2025-10-16 07:00:27 +0800

    自动同步代码 - 2025-10-16 07:00:03

---
+++ generated_code/2025-10/2025-10-16.py
@@ -0,0 +1,23 @@
+def depth_first_search(graph, start):
+    visited = set()
+    stack = [start]
+
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    
+    return visited
+
+# Example usage:
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+print(depth_first_search(graph, 'A'))
