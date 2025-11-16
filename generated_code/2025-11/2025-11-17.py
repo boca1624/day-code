135efe4503426b4de10d55a11b9208de50a9cc7a
qinglong <qinglong@users.noreply.github.com>
2025-11-17 07:00:11 +0800

    提交练习代码 (LeetCode主题) - LeetCode主题: 搜索 - 2025-11-17 07:00:08

---
+++ generated_code/2025-11/2025-11-17.py
@@ -0,0 +1,25 @@
+def depth_first_search(graph, start):
+    visited = set()
+    stack = [start]
+
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+
+    return visited
+
+# Example graph represented as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+# Perform DFS starting from node 'A'
+result = depth_first_search(graph, 'A')
+print(result)
